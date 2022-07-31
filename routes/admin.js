febcb5cbedcf74883ad79ea0ac4b61e99250329e
refitrihidayatullah <refitrihidayatullah19@gmail.com>
2022-07-31 20:03:53 +0700

    update admin and add view bank

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,5 @@ const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/category', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
 module.exports = router;
