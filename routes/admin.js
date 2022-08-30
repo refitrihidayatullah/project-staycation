aef60462551ba04c4f913fde90ab49db8f29fec6
refitrihidayatullah <refitrihidayatullah19@gmail.com>
2022-08-30 09:57:43 +0700

    menyelesaikan delete feature

--- routes/admin.js
+++ routes/admin.js
@@ -24,6 +24,7 @@ router.delete('/item/:id/delete', adminController.deleteItem);
 router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
 router.post('/item/add/feature', upload, adminController.addFeature);
 router.put('/item/update/feature', upload, adminController.editFeature);
+router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 // endpoint booking
 router.get('/booking', adminController.viewBooking);
 module.exports = router;
